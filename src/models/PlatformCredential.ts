96bfc2cf51c5c8c6af67b1e20436ee3976ecf6ed
Pratham Sarankar <pratham@sarankar.com>
2025-07-27 12:15:46 +0530

    Remove enum from platformName

---
+++ src/models/PlatformCredential.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const platformCredentialSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    platformName: {
+      type: String,
+      required: true,
+    },
+    credentials: {
+      type: mongoose.Schema.Types.Mixed,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+platformCredentialSchema.index(
+  { userId: 1, platformName: 1 },
+  { unique: true }
+);
+
+const PlatformCredential = mongoose.model(
+  "PlatformCredential",
+  platformCredentialSchema
+);
+
+export default PlatformCredential;

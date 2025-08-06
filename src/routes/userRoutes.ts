1405fa8ec8af2831e5a91decfe2a97e52c1f9705
Student mr-devloper005 <Student@mr.opxoo5@gmail.com>
2025-08-06 21:50:10 +0530

    feat: add role management to user creation and updates; implement authorization middleware

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-// import { auth } from "../middleware/auth";
+import { auth } from "../middleware/auth";
 import {
   createUser,
   getAllUsers,
@@ -7,20 +7,21 @@ import {
   updateUser,
   deleteUser,
 } from "../controllers/userController";
+import { authorizeRoles } from "../middleware/authorizedRoles";
 
 const router = express.Router();
 
-// router.use(auth);
+router.use(auth);
 
 // Create user 
-router.post("/", createUser);
+router.post("/",authorizeRoles("admin"), createUser);
 // Get all users
-router.get("/", getAllUsers);
+router.get("/",authorizeRoles("admin"), getAllUsers);
 // Get single user by ID
-router.get("/:id", getUserById);
+router.get("/:id",authorizeRoles("admin"), getUserById);
 // Update user by ID
-router.put("/:id", updateUser);
+router.put("/:id",authorizeRoles("admin"), updateUser);
 // Delete user by ID
-router.delete("/:id", deleteUser);
+router.delete("/:id",authorizeRoles("admin"), deleteUser);
 
 export default router;
